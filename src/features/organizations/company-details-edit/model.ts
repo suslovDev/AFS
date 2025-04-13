e6dc58aa604f9220c6068061c7fb57b148a039bb
suslovDev <suslova-valerija@rambler.ru>
2025-04-13 22:09:46 +0300

    fix: useEditDitails; add transformation company-type data before sending to api;

--- src/features/organizations/company-details-edit/model.ts
+++ src/features/organizations/company-details-edit/model.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError } from 'axios';
 import { useEffect, useRef, useState } from 'react';
 import api from '@shared/config/api';
 import { Organization } from '@shared/types';
-import { formatCompanyType } from '@shared/utils';
+import { formatCompanyType, toSnakeCase } from '@shared/utils';
 import { COMPANY_API } from '../api';
 
 export const updateOrganization = async (id: string, data: Partial<Organization>) => {
@@ -42,7 +42,7 @@ export const useEditDitails = (details: Organization) => {
   const ditailsToUpdate: Partial<Organization> = {
     businessEntity: entity[0],
     contract: { issue_date: date, no: contractNumber },
-    type: companyType,
+    type: toSnakeCase(companyType),
   };
 
   const updateDetails = () => updateOrganization(id, ditailsToUpdate);
